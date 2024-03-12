5b6787f6bbf7a148558d5fd1f277f7d0bbbb52f2
David Lima <antdavidlima@gmail.com>
2024-03-11 23:59:57 -0300

    eww: simplify config and change theme to gruvbox

--- eww/.config/eww/modules/get-workspaces.sh
+++ eww/.config/eww/modules/get-workspaces.sh
@@ -4,7 +4,7 @@ spaces (){
 	hyprctl workspaces -j | jq 'map({id: .id | tostring}) | sort_by(.id)' -Mc
 }
 
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r; do
 	spaces
 done
 
