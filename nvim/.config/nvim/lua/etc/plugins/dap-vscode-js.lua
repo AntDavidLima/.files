aecda3b8db591286f0ef92cefe2faa9d2e5f25d0
David Lima <antdavidlima@gmail.com>
2024-03-11 23:52:28 -0300

    nvim: pin dap-vscode-js version

--- nvim/.config/nvim/lua/etc/plugins/dap-vscode-js.lua
+++ nvim/.config/nvim/lua/etc/plugins/dap-vscode-js.lua
@@ -13,6 +13,7 @@ M.config = function()
 	require("dap-vscode-js").setup({
 		debugger_path = "/home/david/.local/share/nvim/lazy/vscode-js-debug",
 		adapters = { "pwa-node" },
+		pin = true,
 	})
 
 	for _, language in ipairs({ "typescript", "javascript" }) do
