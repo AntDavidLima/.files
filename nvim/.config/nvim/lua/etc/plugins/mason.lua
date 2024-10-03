c10d6d0782df6e94b1d7c3eb70679c8707b0320d
David Lima <antdavidlima@gmail.com>
2024-10-03 08:38:09 -0300

    feat(mason): config python development

--- nvim/.config/nvim/lua/etc/plugins/mason.lua
+++ nvim/.config/nvim/lua/etc/plugins/mason.lua
@@ -21,9 +21,12 @@ M.config = function()
 			"docker_compose_language_service",
 			"eslint",
 			"bashls",
+			"basedpyright",
 			"biome",
+			"ruff"
 		},
 	})
+
 	require("mason-lspconfig").setup_handlers({
 		function(server_name)
 			---@diagnostic disable-next-line: undefined-field
@@ -31,6 +34,24 @@ M.config = function()
 				capabilities = require("cmp_nvim_lsp").default_capabilities(),
 			})
 		end,
+
+		["basedpyright"] = function()
+			local function which_python()
+				local f = io.popen('env which python', 'r') or error("Fail to execute 'env which python'")
+				local s = f:read('*a') or error("Fail to read from io.popen result")
+				f:close()
+				return string.gsub(s, '%s+$', '')
+			end
+
+			require("lspconfig").basedpyright.setup({
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				settings = {
+					python = {
+						pythonPath = which_python(),
+					}
+				}
+			})
+		end
 	})
 
 	vim.keymap.set("n", "<Space>m", ":Mason<CR>")
