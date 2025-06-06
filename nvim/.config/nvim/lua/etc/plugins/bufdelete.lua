8a36ff48a82b4e430e555c31200cdca476969a28
David Lima <antdavidlima@gmail.com>
2025-06-06 11:28:46 -0300

    replace nvim config with kickstart-modular nvim config

--- nvim/.config/nvim/lua/etc/plugins/bufdelete.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-	'famiu/bufdelete.nvim',
-}
-
-M.config = function()
-	vim.keymap.set('n', 'Q', vim.cmd.Bdelete)
-end
-
-return M
--- nvim/.config/nvim/lua/etc/plugins/neodev.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-	'folke/neodev.nvim',
-	opts = {},
-	dependencies = {
-		'neovim/nvim-lspconfig',
-	},
-}
-
-return M
